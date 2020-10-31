8592c53e3ad82e5a91fe3a97ae35184de133302d
maks-nurgazy <maksnurgazy@gmail.com>
2020-11-01 01:09:01 +0600

    starting with material ui

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CVmakerContextProvider from "./contexts/CVmakerContext";
+import Navbar from "./components/Navbar";
+import Resume from "./components/Resume";
 
 function App(props) {
     return (
         <CVmakerContextProvider>
-            <h1>Hello Mir</h1>
+            <Navbar />
+            <Resume />
         </CVmakerContextProvider>
     )
 }
---
+++ assets/js/components/Resume.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
+
+export default function Resume() {
+    return (
+        <React.Fragment>
+            <CssBaseline/>
+            <Container maxWidth="md">
+
+            </Container>
+        </React.Fragment>
+    );
+}
